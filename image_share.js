0f1a06afef7650a53f9ca7849b3ea957d80a6efb
Vargentum <vargentum@gmail.com>
2016-06-28 11:00:48 +0300

    1.7 add twbs package

--- image_share.js
+++ image_share.js
@@ -1,5 +1,16 @@
 if (Meteor.isClient){
+  var imgData = {
+    images: [{
+      src: 'icon-dark.jpg',
+      alt: 'image with dark background'
+    },
+    {
+      src: 'icon-white.jpg',
+      alt: 'image with white background'
+    }]
+  }
 
+  Template.images.helpers(imgData)
 }
 
 if (Meteor.isServer){
