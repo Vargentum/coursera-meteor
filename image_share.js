cb9fe5d76b3006ff626ec1d8e3b4428359d94a35
Vargentum <vargentum@gmail.com>
2016-07-05 21:30:15 +0300

    2.3 implement deleting

--- image_share.js
+++ image_share.js
@@ -9,6 +9,11 @@ if (Meteor.isClient){
   Template.images.events({
     'click .js-image': function({target}) {
       $(target).css('width', '50px')
+    },
+    'click .js-del-img': function(ev) {
+      $(`#${this._id}`).hide('slow', () => 
+        Images.remove({_id: this._id}) // this refers to Template data (model ?)
+      )
     }
   })
 }
